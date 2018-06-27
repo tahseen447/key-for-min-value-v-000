59eae18e456052c320d859fa4804c0239220ef1d
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-27 06:00:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
-      min_key = key
+      min_key << key
     end
   end
   min_key

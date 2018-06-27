f88dedfdf439a3725096318f2b726c66c5cf7519
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-27 06:01:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 0
   min_key = ""
+  if name_hash.length > 0
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
@@ -11,4 +12,7 @@ def key_for_min_value(name_hash)
     end
   end
   min_key
+end
+else
+  nil
 end
